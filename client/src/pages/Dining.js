7cebea1148390bbd840c210f0b4311e0e195f9f1
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-02-11 21:12:09 -0600

    updates to dining component page

---
+++ client/src/pages/Dining.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Dining from "../components/Dining"
+
+class SearchDining extends component {
+
+}
+
+export default SearchDining
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 require("dotenv").config();
 
-const BASEURL = "https://api.yelp.com/v3/businesses/search";
+const BASEURL = "https://api.yelp.com/v3/businesses/search;
 // const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 
 export default {
